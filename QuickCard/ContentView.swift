a7450fe24c37531d79c5c78e754ae370242cff94
daferpi <daferpi@gmail.com>
2020-11-01 12:40:14 +0100

    created inicial user card

--- QuickCard/ContentView.swift
+++ QuickCard/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 300, height: 200)
+                .cornerRadius(20)
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                Text("Jonathan Wong")
+                    .font(.title)
+                Text("Senior Software engineer")
+                    .italic()
+                Spacer()
+                HStack {
+                    Image(systemName: "envelope.fill")
+                    .foregroundColor(.orange)
+                    Text("mobileunder10@gmail.com")
+                }
+                HStack {
+                    Image(systemName: "link")
+                        .foregroundColor(.orange)
+                    Text("mobileunder10.com")
+
+                }
+                HStack {
+                    Image(systemName: "location.fill")
+                        .foregroundColor(.orange)
+                    Text("12345 Highland Road")
+                }
+            }.padding()
+            .foregroundColor(.white)
+        }.frame(width: 300, height: 200)
     }
 }
 

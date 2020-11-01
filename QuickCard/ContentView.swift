d1f2349e6f23f2cfd4a9f4987d8e01ada945e518
daferpi <daferpi@gmail.com>
2020-11-01 13:08:40 +0100

    add image label component

--- QuickCard/ContentView.swift
+++ QuickCard/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Rectangle()
-                .fill(Color.blue)
+                .fill(Color("CardBackground"))
                 .frame(width: 300, height: 200)
                 .cornerRadius(20)
                 .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
@@ -21,22 +21,12 @@ struct ContentView: View {
                 Text("Senior Software engineer")
                     .italic()
                 Spacer()
-                HStack {
-                    Image(systemName: "envelope.fill")
-                    .foregroundColor(.orange)
-                    Text("mobileunder10@gmail.com")
-                }
-                HStack {
-                    Image(systemName: "link")
-                        .foregroundColor(.orange)
-                    Text("mobileunder10.com")
-
-                }
-                HStack {
-                    Image(systemName: "location.fill")
-                        .foregroundColor(.orange)
-                    Text("12345 Highland Road")
-                }
+                ImageLabelView(imageName: "envelope.fill",
+                               text: .constant("mobileunder10@gmail.com"))
+                ImageLabelView(imageName: "link",
+                               text: .constant("mobileunder10.com"))
+                ImageLabelView(imageName: "location.fill",
+                               text: .constant("12345 Highland Road"))
             }.padding()
             .foregroundColor(.white)
         }.frame(width: 300, height: 200)
---
+++ QuickCard/ImageLabelView.swift
@@ -0,0 +1,28 @@
+//
+//  ImageLabelView.swift
+//  QuickCard
+//
+//  Created by Abel Fernandez Pineiro on 01/11/2020.
+//
+
+import SwiftUI
+
+struct ImageLabelView: View {
+    var imageName: String
+    @Binding var text: String
+
+    var body: some View {
+        HStack {
+            Image(systemName: imageName)
+                .foregroundColor(.orange)
+            Text(text)
+        }
+    }
+}
+
+struct ImageLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageLabelView(imageName: "envelope.fill",
+                       text: .constant("mobileunder10@gmail.com"))
+    }
+}

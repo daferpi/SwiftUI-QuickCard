50f6f81093af0a3eedb2dcd722b0316ee0f3b26f
Abel Fernandez <abel.fernandez@adevinta.com>
2020-11-01 12:14:08 +0100

    Initial Commit

---
+++ QuickCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickCard
+//
+//  Created by Abel Fernandez Pineiro on 01/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickCard/QuickCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCardApp.swift
+//  QuickCard
+//
+//  Created by Abel Fernandez Pineiro on 01/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct QuickCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
